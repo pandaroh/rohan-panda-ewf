2be6e57157b3924961aef905d820d5f9b3a44d37
kanchan shrestha <kanchan@kanchans-MacBook-Air.local>
2021-06-08 10:13:55 -0400

    Add templates

--- app.py
+++ app.py
@@ -1,10 +1,40 @@
-from flask import Flask
+from flask import Flask, render_template
+from requests import Request, Session
+import json
+
+class Crypto:
+
+    def get_top_5(self):
+
+        url = 'https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest'
+        parameters = {
+          'start':'1',
+          'limit':'5',
+          'convert':'USD'
+        }
+        headers = {
+          'Accepts': 'application/json',
+          'X-CMC_PRO_API_KEY': '6c51348a-7cd7-49b5-9504-fa6b5e6ada84',
+        }
+
+        session = Session()
+        session.headers.update(headers)
+
+        response = session.get(url, params=parameters)
+        data = json.loads(response.text)
+        return data['data']
 
 app = Flask(__name__)
+crypto = Crypto()
 
 @app.route("/")
-def index():
-     return "Hello world"
+def hello():
+    results = crypto.get_top_5()
+
+    for result in results:
+        result['quote']['USD']['price'] = '$ ' + "{:.2f}".format(result['quote']['USD']['price'])
+
+    return render_template('index.html', **locals())
 
 if __name__ == "__main__":
-     app.run()
+    app.run(host='0.0.0.0')
